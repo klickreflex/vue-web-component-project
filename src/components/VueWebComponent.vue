e49c8beeb949e390e77d1250ff4e0539e9d94342
Daniel Wentsch <hello@wentsch.me>
2020-04-16 17:33:37 +0200

    Make vue component distributable as web component

---
+++ src/components/VueWebComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <h1>My Vue Web Component</h1>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['msg']
+  }
+</script>
+
+<style scoped>
+    .box {
+        border: 3px dotted rebeccapurple;
+        padding: 16px;
+    }
+
+    h1 {
+        color: lightgreen;
+        font-weight: bold;
+        font-size: 58px;
+    }
+
+    div {
+        font-family: monospace;
+        font-size: 18px;
+    }
+</style>
